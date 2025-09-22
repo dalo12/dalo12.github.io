cac9751875a91cbad0e6a8737046802195587645
David López <dalo_vidpez@hotmail.com.ar>
2025-09-22 18:59:55 -0300

    Definido fragmento para micro frontend

---
+++ fragmento.js
@@ -0,0 +1,13 @@
+class BadFilmComponent extends HTMLElement {
+    constructor(){
+        super();
+        let title = "Happy Gilmore 2";
+        let img = "https://www.imdb.com/es/title/tt31868189/mediaviewer/rm1674557698/?ref_=tt_ov_i"
+        this.innerHTML = `
+            <h2>${title}<h2>
+            <img src="${img}" width="100" height="300">
+        `;
+    }
+}
+
+customElements.define("dalo", BadFilmComponent)
